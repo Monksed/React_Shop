f217b51f2c997b66cd617ac664e30b40ea1eb13e
Denis <routineiseverywhere@gmail.com>
2025-12-10 22:49:16 +0500

    ✨User: Controller + DTO

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+using CoreData.Contexts;
+using CoreData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopBackend.DTO;
+namespace ShopBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly React_ShopContext _context;
+        public UserController(React_ShopContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("user/{id}")]
+        public async Task<IActionResult> GetOneUser(Guid id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null)
+                return NotFound(new { message = "������������ �� ������" });
+            var userDto = new UserDTO()
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Score = user.Score,
+                Fio = user.Fio,
+                Email = user.Email,
+                Phone = user.Phone,
+            };
+
+            return Ok(userDto);
+        }
+
+        [HttpPost("UpdateUser")]
+        public async Task<IActionResult> UpdateUser([FromBody] UserDTO dto)
+        {
+            var user = await _context.Users.Where(x => x.Id == dto.Id).FirstOrDefaultAsync();
+
+            if (user == null) return BadRequest(new { error = true, message = "������������ �� ������" });
+
+            user.Email = dto.Email;
+            user.Phone = dto.Phone;
+            user.Fio = dto.Fio;
+            user.Username = dto.Username;
+            user.UpdateDate = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Unspecified);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ backend/DTO/UserDTO.cs
@@ -0,0 +1,13 @@
+namespace ShopBackend.DTO;
+
+public class UserDTO
+{
+    public Guid Id { get; set; }
+    public string? Username { get; set; }
+    public long? Score { get; set; }
+    public string? Fio { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+}
+
+
